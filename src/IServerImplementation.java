b26999a61fead86958359b4e036eeecb9a416272
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-05-03 11:09:04 +0200

    init

---
+++ src/IServerImplementation.java
@@ -0,0 +1,11 @@
+﻿import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import javax.script.ScriptException;
+
+
+public interface IServerImplementation extends Remote
+{	
+	public boolean checkForValidUserInput(String input) throws RemoteException;	
+	public String createClearStringWithoutParanthesis(String input) throws RemoteException, ScriptException;
+}
